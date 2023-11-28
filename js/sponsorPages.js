0801a22abd6c5b388daf26d12c055ed6d5668693
Sam Austin <samuelaa02@gmail.com>
2023-11-27 21:59:47 -0500

    updated all driver pages to use globalv2.css; began implementing user management for sponsors

--- js/sponsorPages.js
+++ js/sponsorPages.js
@@ -1,10 +1,10 @@
 async function loadDashboard(){
     try{
         const result = await validateToken('dashboard');
-        document.getElementById("user-name").innerHTML = "Welcome, " + result.fName;
+        document.getElementById("user-name").innerHTML = result.fName;
         document.getElementById("sponsor-name").innerHTML = result.sponsor;
-        document.getElementById("driver-count").innerHTML = "Encouraging "+ result.driverCount+" drivers to drive safely!";
-        document.getElementById("spon-desc").innerHTML = result.sponsorDesc;
+        document.getElementById("driver-count").innerHTML =result.driverCount;
+        document.getElementById("sponsor-desc").innerHTML = result.sponsorDesc;
         
     }
     catch(error){
@@ -28,4 +28,29 @@ async function loadManagement(){
     document.getElementById("loading").style.display = 'none';
     document.getElementById("content").style.display = 'flex';
     return;
+}
+
+async function searchForUser(){
+    try{
+        //get user data and send to api
+        
+        var userType = document.getElementById("search-user-type").value;
+        var userDiscrim = document.getElementById("search-user-identity").value;
+        var userTerm = document.getElementById("search-user-string").value;
+
+        //if fields are filled
+        if(userType && userDiscrim && userTerm){
+            //send to api
+            //succesfful match
+                //populate
+                switchContent("none","user-result","flex");
+
+        }
+        else{
+            alert("Please supply all search terms.")
+        }
+    }
+    catch(error){
+        console.error("User search error",error);
+    }
 }
