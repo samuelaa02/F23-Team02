a9bed041f70f3747ea17c2351dad1f244fbdadf5
mbaldiz23 <mbaldiz@clemson.edu>
2023-12-01 19:34:24 -0500

    guess what, more sponsor management changes

--- js/userSearch.js
+++ js/userSearch.js
@@ -17,8 +17,11 @@ async function getUser(){
     var lName = data.result.LastName;
     var phone = data.result.Phone;
     var password = data.result.Password;
+    var points = data.result.Points;
 
     document.getElementById("user-result").style.visibility = "visible";
+    switchContent("none","user-result","flex");
+
     document.getElementById("search-term").innerHTML = userName;
     document.getElementById("result-usertype").innerHTML = userType;
     document.getElementById("result-username").innerHTML = username;
@@ -27,6 +30,14 @@ async function getUser(){
     document.getElementById("result-lName").innerHTML = lName;
     document.getElementById("result-phone").innerHTML = phone;
     document.getElementById("result-password").innerHTML = password;
+    document.getElementById("result-points").innerHTML = points;
+
+    document.getElementById("edit-username").placeholder = username;
+    document.getElementById("edit-email").placeholder = email;
+    document.getElementById("edit-fName").placeholder = fName;
+    document.getElementById("edit-lName").placeholder = lName;
+    document.getElementById("edit-phone").placeholder = phone;
+    document.getElementById("edit-password").placeholder = password;
   }
   else{
       alert("Error retrieving user information");
