c65e084c454ebaf7898aa7700ce045b789bd731d
mbaldiz23 <mbaldiz@clemson.edu>
2023-12-01 19:13:54 -0500

    update sponsor_management even further

--- js/userSearch.js
+++ js/userSearch.js
@@ -1,21 +1,35 @@
-"use strict";
+async function getUser(){
+  var userType = document.getElementById("userType").value;
+  var searchType = document.getElementById("searchType").value;
+  var userName = document.getElementById("userName").value;
 
-window.onload = function (){
-    getUsers();
+  var queryParams = "?userType="+userType+"&searchType="+searchType+"&user="+userName;
+
+  const res = await fetch('https://u76zsrtgq8.execute-api.us-east-1.amazonaws.com/team02-testing/UserSearch'+queryParams)
+  const data = await res.json();
+  console.log(data);
+
+  if(data.status === "Success"){
+    //refresh data on page
+    var username = data.result.Username;
+    var email = data.result.Email;
+    var fName = data.result.FirstName;
+    var lName = data.result.LastName;
+    var phone = data.result.Phone;
+    var password = data.result.Password;
+
+    document.getElementById("user-result").style.visibility = "visible";
+    document.getElementById("search-term").innerHTML = userName;
+    document.getElementById("result-usertype").innerHTML = userType;
+    document.getElementById("result-username").innerHTML = username;
+    document.getElementById("result-email").innerHTML = email;
+    document.getElementById("result-fName").innerHTML = fName;
+    document.getElementById("result-lName").innerHTML = lName;
+    document.getElementById("result-phone").innerHTML = phone;
+    document.getElementById("result-password").innerHTML = password;
+  }
+  else{
+      alert("Error retrieving user information");
+  }
+
 }
-
-function getUsers(){
-    fetch ('https://u76zsrtgq8.execute-api.us-east-1.amazonaws.com/UserSearch').then(res => {
-        return res.json();
-    })
-    .then(data => {
-        console.log(data);
-        document.getElementById("project-name").innerHTML = data.ProductName;
-        document.getElementById("team-num").innerHTML = data.TeamNum;
-        document.getElementById("version").innerHTML = data.Version;
-        document.getElementById("project-desc").innerHTML = data.ProductDesc;
-        document.getElementById("release").innerHTML = data.ReleaseDate.slice(0,10);
-    })
-    .catch(error => console.log(error));
-}
-
